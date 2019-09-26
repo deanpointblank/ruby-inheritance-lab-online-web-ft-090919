e94f52222d23fb802554df4d93149e46703e476e
Dean L Betty <deanlloyd64@gmail.com>
2019-09-26 03:03:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User
+  attr_accessor :knoweldge
+  
+  def initialize
+    @knoweldge = []
+  end
 
+  def learn
+    @knoweldge << Teacher.teach
+  end
+  
+  def knoweldge
+    @knoweldge
+  end
 end
